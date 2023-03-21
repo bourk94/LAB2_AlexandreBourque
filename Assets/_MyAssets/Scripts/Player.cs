c897b1263ee155b92309ffd7bedd3a6fad465853
bourk94 <113631191+bourk94@users.noreply.github.com>
2023-03-21 12:55:43 -0400

    Initial commit

---
+++ Assets/_MyAssets/Scripts/Player.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    //Attribut
+    [SerializeField] private float _vitesse = 600;
+    private Vector3 _positionDepart = new Vector3(-44f, 0.98f, -44f);
+    private Rigidbody _rb;
+
+    //Methodes privees
+    private void Start()
+    {
+        //Postion de depart du joueur
+        this.transform.position = _positionDepart;
+        _rb = GetComponent<Rigidbody>();
+
+    }
+
+    private void FixedUpdate()
+    {
+        MouvementsJoueur();
+    }
+
+    private void MouvementsJoueur()
+    {
+        float positionX = Input.GetAxis("Horizontal");
+        float positionZ = Input.GetAxis("Vertical");
+
+        Vector3 direction = new Vector3(positionX, 0f, positionZ);
+        _rb.velocity = direction * Time.fixedDeltaTime * _vitesse;
+    }
+}
