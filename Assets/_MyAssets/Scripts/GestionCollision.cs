199d8075cb53a61f6f1897ef813c9b274e41f092
bourk94 <113631191+bourk94@users.noreply.github.com>
2023-03-22 11:55:29 -0400

    Niveau1 terminer

--- Assets/_MyAssets/Scripts/GestionCollision.cs
+++ Assets/_MyAssets/Scripts/GestionCollision.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Drawing;
 using UnityEngine;
 
 public class GestionCollision : MonoBehaviour
@@ -7,6 +8,10 @@ public class GestionCollision : MonoBehaviour
     // Attributs
     private GameManager _gameManager;
     private bool _toucher;
+    private float _delais = 3;
+    private float _tempsDepart;
+    private UnityEngine.Color _color;
+
 
     private void Start()
     {
@@ -14,13 +19,25 @@ public class GestionCollision : MonoBehaviour
         _toucher = false;
     }
 
-    private void OnCollisionEnter(Collision collision)
+    private void Update()
     {
+        // Si l'obstacle est touché et que le délais est dépassé, on remet la couleur de base
+        if (_toucher && Time.time > _tempsDepart + _delais)
+        {
+            gameObject.GetComponent<MeshRenderer>().material.color = _color;
+            _toucher = false;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {      
         if (!_toucher && collision.gameObject.tag == "Player")
         {
-            gameObject.GetComponent<MeshRenderer>().material.color = Color.red;
+            _color = gameObject.GetComponent<MeshRenderer>().material.color;
+            _tempsDepart = Time.time;
+            gameObject.GetComponent<MeshRenderer>().material.color = UnityEngine.Color.red;
             _gameManager.AugmenterPointage();
-            _toucher = true;
-        }
+            _toucher = true;            
+        }  
     }
 }
